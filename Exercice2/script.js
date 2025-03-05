5ae8b8e342ebb97b5694859b663e198cec9d6350
RobinChappalley <robinchappalley@gmail.com>
2025-03-06 00:46:36 +0100

    add function to algien them on click

--- Exercice2/script.js
+++ Exercice2/script.js
@@ -24,3 +24,35 @@ const thirdCircle = select("svg")
 thirdCircle.attr("fill", "peachpuff");
 firstCircle.attr("cx", 100);
 secondCircle.attr("cx", 200);
+
+firstCircle.text("Hello");
+
+select("svg")
+  .append("text")
+  .attr("x", 10)
+  .attr("y", 100)
+  .text("Premier cercle");
+
+select("svg")
+  .append("text")
+  .attr("x", 200)
+  .attr("y", 200)
+  .text("Deuxième cercle");
+
+select("svg")
+  .append("text")
+  .attr("x", 300)
+  .attr("y", 300)
+  .text("Troisième cercle");
+
+thirdCircle.on("click", () => {
+  firstCircle.attr("cx", "250");
+  secondCircle.attr("cx", "250");
+  thirdCircle.attr("cx", "250");
+});
+
+const array = [20, 5, 25, 8, 15];
+
+array.forEach((element) => {
+  console.log(element);
+});
