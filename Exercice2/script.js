01e4773ec1a6ea0e3b65f74c50fff50853f29118
RobinChappalley <robinchappalley@gmail.com>
2025-03-06 00:35:45 +0100

    Created circles

--- Exercice2/script.js
+++ Exercice2/script.js
@@ -1,3 +1,26 @@
-import {select} from "d3-selection";
+import { select } from "d3-selection";
 
-// C'est ici que vous allez écrire les premières lignes avec d3.js!
+const firstCircle = select("svg")
+  .append("circle")
+  .attr("cx", 50)
+  .attr("cy", 50)
+  .attr("r", 40)
+  .attr("fill", "red");
+
+const secondCircle = select("svg")
+  .append("circle")
+  .attr("cx", 150)
+  .attr("cy", 150)
+  .attr("r", 40)
+  .attr("fill", "red");
+
+const thirdCircle = select("svg")
+  .append("circle")
+  .attr("cx", 250)
+  .attr("cy", 250)
+  .attr("r", 40)
+  .attr("fill", "red");
+
+thirdCircle.attr("fill", "peachpuff");
+firstCircle.attr("cx", 100);
+secondCircle.attr("cx", 200);
